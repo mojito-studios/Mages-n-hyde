f934e3afb9cb9929691a9ef4f8ed506cb63e8ef6
SandraMartin1506 <sandramcmp@gmail.com>
2024-09-27 21:06:40 +0200

    Intento de movimiento

---
+++ JuegosEnWeb/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : NetworkBehaviour
+{
+    private int numPlayers = 4;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
