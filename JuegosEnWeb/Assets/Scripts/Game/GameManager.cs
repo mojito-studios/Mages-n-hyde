260d4324b0eaec8fa11996635668c7ba4c7cf1aa
SandraMartin1506 <sandramcmp@gmail.com>
2024-10-24 23:47:51 +0200

    Revert "PowerUps aparecen"
    This reverts commit ba9456875da4913aae7a03f8c8ea68233ff599f1.

--- JuegosEnWeb/Assets/Scripts/Game/GameManager.cs
+++ JuegosEnWeb/Assets/Scripts/Game/GameManager.cs
@@ -6,59 +6,17 @@ using UnityEngine.InputSystem;
 
 public class GameManager : NetworkBehaviour 
 {
-    //private int numPlayers = 4;
-    [SerializeField] private GameObject puPrefab;
-    [HideInInspector] public int puInScene = 0;
-    private const int MaxPU = 5;
-    public static GameManager Instance { get; private set; }
+    private int numPlayers = 4;
 
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-    }
     void Start()
     {
-        NetworkManager.Singleton.OnServerStarted += SpawnPUStart;
+        
     }
 
-    
+
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void SpawnPUStart()
-    {
-        NetworkManager.Singleton.OnServerStarted -= SpawnPUStart;
-        StartCoroutine(SpawnOverTime());
-    }
-
-    private void SpawnPU()
-    {
-        GameObject instance = Instantiate(puPrefab, GetRandomPosition(), Quaternion.identity);
-        NetworkObject instanceNetworkObject = instance.GetComponent<NetworkObject>();
-        instanceNetworkObject.Spawn();
-        puInScene++;
-        
-    }
-    
-    private Vector3 GetRandomPosition()
-    {
-        return new Vector3(Random.Range(-8f, 8f), Random.Range(-3f, 3f), 0); //Ajustarlo luego bien al mapa esto es solo como prueba
-    }
-
-    private IEnumerator SpawnOverTime()
-    {
-        while(NetworkManager.Singleton.ConnectedClients.Count > 1) //No cuenta al host
-        {
-            yield return new WaitForSeconds(10f);
-            if(puInScene < MaxPU)
-                SpawnPU();
-        }
     }
 }
